ae20735d5487653dbf9c986665ab64281d6406a0
Bhagyapiyumali <166277137+Bhagyapiyumali@users.noreply.github.com>
2025-12-07 23:59:47 +0530

    Update server port configuration from 3000 to 5000

--- server/app.js
+++ server/app.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import chatbotRoute from './routes/chatbotRoute.js';
 
 
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
